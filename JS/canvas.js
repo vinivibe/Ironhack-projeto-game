4648d86387ed0c87f17b215a8f1e8924ac05df91
Heitor Godau <heitorgodau@gmail.com>
2019-12-12 12:36:11 -0300

    Some animation, bgImage and minor changes

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,132 +1,82 @@
+const clearCanvas = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-
-function fundo(){
-  if(frame >= 0 && frame <= 1500){
-    fundoImg.src = "Img/fundo.jpg";
-    context.drawImage(fundoImg, 0, 0); 
-  } else if(frame > 1500 && frame <= 2500){
-    fundodois.src = "Img/fundo5.png";
-    context.drawImage(fundodois, 0, 0); 
-  } else if (frame > 2500 && frame <= 3500 ) {
-    fundotres.src = "Img/fundo6.png";
-    context.drawImage(fundotres, 0, 0); 
-  } else if (frame > 3500 && frame <= 4500) {
-    fundoquatro.src = "Img/fundo8.jpg";
-    context.drawImage(fundoquatro, 0, 0);
-  } else if (frame > 4500 && frame <= 5000){
-    fundocinco.src = "Img/fundo9.jpg";
-    context.drawImage(fundocinco, 0, 0);
+function fundo() {
+  if (frame <= 1500) {
+    canvas.style.backgroundImage = 'url("Img/fundo.jpg")';
+  } else if (frame <= 2500) {
+    canvas.style.backgroundImage = 'url("Img/fundo5.png")';
+  } else if (frame <= 3500) {
+    canvas.style.backgroundImage = 'url("Img/fundo6.png")';
+  } else if (frame <= 4500) {
+    canvas.style.backgroundImage = 'url("Img/fundo8.jpg")';
+  } else if (frame <= 5000) {
+    canvas.style.backgroundImage = 'url("Img/fundo9.jpg")';
   } else {
-    fundodez.src = "Img/fundo10.png";
-    context.drawImage(fundodez, 0, 0);
-    mortalkombat.src = "Img/Toasty.png";
+    canvas.style.backgroundImage = 'url("Img/fundo10.png")';
+    mortalkombat.src = 'Img/Toasty.png';
     context.drawImage(mortalkombat, 600, 40);
-}
-}
-
-function drawLife(){
-    
-  // context.font = "25px serif";
-  // context.fillStyle = "black";
-  // context.fillText("Life: " + life, 100, 50);
-
-
-  if(life === 1){
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,50,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,100,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,150,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,200,30)
-  } else if (life === 2){
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,50,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,100,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,150,30)
-    lifeImg.src='Img/hearts.png';
-  } else if (life === 3){
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,50,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,100,30)
-    lifeImg.src='Img/hearts.png';
-  } else if (life === 4){
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,50,30)
-    lifeImg.src='Img/hearts.png';
-  } else {
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,50,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,100,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,150,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,200,30)
-    lifeImg.src='Img/hearts.png';
-    context.drawImage(lifeImg,250,30)
   }
-   
+}
+
+function drawLife() {
+  let heartX = 50;
+  for (let i = 0; i < life; i += 1) {
+    lifeImg.src='Img/hearts.png';
+    context.drawImage(lifeImg, heartX, 30);
+    heartX += 50;
+  }
 }
 
 function score() {
   var points = Math.floor(frame / 10);
-  context.font = "29px fantasy";
-  context.fillStyle = "black";
-  context.fillText("SCORE  " + points, 600, 50);
+  context.font = '29px fantasy';
+  context.fillStyle = 'black';
+  context.fillText('SCORE  ' + points, 600, 50);
 }
 
 
-  function drawGameOver(){
-    gameOverimg.src='Img/gameover.png';
-    context.drawImage(gameOverimg, 0, 0)
-  }
+function drawGameOver() {
+  gameOverimg.src='Img/gameover.png';
+  context.drawImage(gameOverimg, 0, 0);
+}
 
-let ninja = new Ninja(300,400,100,109);
+let ninja = new Ninja(300, 400, 100, 109);
 
 
 document.onkeydown = function(e) {
-switch (e.keyCode) {
-        case 38: // up arrow
-        //   ninja.eixoY -= 2
-        //   console.log('up')
-          break;
-        case 40: // down arrow
-        //   ninja.eixoY += 2;
-        // console.log('down')
-          break;
-        case 37: // left arrow
-        if (ninja.eixoX < 8){
-            ninja.eixoX = 5;
-            // console.log('Bateu!')
-        } else {
-            ninja.eixoX -= 30;
-        } 
-        // console.log('left')
-          break;
-        case 39: // right arrow
-        if(ninja.eixoX + ninja.width >= canvas.width){
-            ninja.eixoX = canvas.width - ninja.width;
-            // console.log('Bateu!')
-        // } else if (ninja.eixoX > (enemy.eixoX -47)){
-        //     ninja.eixoX - enemy.eixoY;
-        }
-        else {
-            ninja.eixoX += 10;
-        } 
-        // console.log('right')
-          break;
-        case 32 : //space
-        attack()
-        attackBIG()  
-
-        break;
+  switch (e.keyCode) {
+    // case 38:  // up arrow
+      //ninja.eixoY -= 2
+      // break;
+    // case 40: // down arrow
+      // ninja.eixoY += 2;
+      // break;
+    case 37: // left arrow
+      if (ninja.eixoX < 8){
+        ninja.eixoX = 5;
+      } else {
+        ninja.eixoX -= 30;
       }
+      break;
+    case 39: // right arrow
+      if(ninja.eixoX + ninja.width >= canvas.width){
+        ninja.eixoX = canvas.width - ninja.width;
+      // } else if (ninja.eixoX > (enemy.eixoX -47)){
+      //     ninja.eixoX - enemy.eixoY;
+      } else {
+        ninja.eixoX += 10;
       }
+      break;
+    case 32 : //space
+      attack();
+      attackBIG();
+      break;
+    case 80: //P
+      pauseGame();
+  }
+}
 
       function machineZumbi(){
         if(frame % 120 === 0){
@@ -137,7 +87,9 @@ switch (e.keyCode) {
           zumbi.moving();
           if(zumbi.eixoX < 0 - zumbi.width){
               enemies.splice(idx,1)
-              life +=1
+              if (life > 0) {
+                life -=1
+              }
 
           }
       })
@@ -153,40 +105,45 @@ switch (e.keyCode) {
           bigzumbi.movingg();
           if(bigzumbi.eixoX < -50){
               bigenemies.splice(idx,1)
-              life +=1
+              if (life > 0) {
+                life -=1
+              }
           }
       })
       }
-let count = 0
-function attack(){
-  setInterval(() => {
+let count = 0;
+let attackStatus = false;
+
+function attack() {
+  attackStatus = true;
+  const intervalId = setInterval(() => {
     if (count <= 9) {
-      ninja.drawAttack(count);
+      // attackImg.src = `Img/Attack${count}.png`;
+      // ninja.drawPlayer(attackImg);
+      // console.log(attackImg);
       count += 1;
     } else {
-      clearInterval();
+      clearInterval(intervalId);
+      attackStatus = false;
     }
-  }, 1000)
+  }, 40);
   count = 0;
-  enemies.forEach(function(zumbi,idx){
-    if(zumbi.eixoX <= ninja.eixoX +120 && zumbi.eixoX >= ninja.eixoX +40){
-      zumbi.receiveDamage()
-      // console.log(zumbi.life)
-      // console.log(attackstatus)
+  // setTimeout(() => {
+  //   attackStatus = false;
+  // }, 50);
+  enemies.forEach((zumbi) => {
+    if (zumbi.eixoX <= ninja.eixoX + 120 && zumbi.eixoX >= ninja.eixoX + 40) {
+      zumbi.receiveDamage();
     }
   });
 }
 
-function attackBIG(){
-  attackstatus = true;
+function attackBIG() {
   bigenemies.forEach((bigzumbi,idx) => {
   if(bigzumbi.eixoX <= ninja.eixoX +120 && bigzumbi.eixoX >= ninja.eixoX +40){
      bigzumbi.receiveDamage()
-    //  console.log(bigzumbi.life)
-    //  console.log(attackstatus)
   }
 });
-  // attackstatus = false;
 }
 
 function death(){
@@ -206,36 +163,52 @@ function deathBigZumbi(){
   })
 }
 
-
-  function gameOver(){
-  enemies.forEach(function(zumbi,idx){
-    // console.log(zumbi.eixoX)
-  });
-  if (life >= 5){
-    drawGameOver()
-    return
+function gameOver() {
+  if (life <= 0) {
+    drawGameOver();
+    setTimeout(() => cancelAnimationFrame(myReq), 200);
+  }
+}
+let pause = false;
+const pauseGame = () => {
+  pause = !pause;
+  if (pause) {
     cancelAnimationFrame(myReq);
+  } else {
+    requestAnimationFrame(updateGameArea);
   }
 }
 
-
-
-
 const updateGameArea = () => {
-    fundo();
-    drawLife();
+  clearCanvas();
+  fundo();
+  drawLife();
+  if (!attackStatus) {
     ninja.drawPlayer(playerImg);
-    machineBigZumbi()
-    machineZumbi()
-    frame += 1;
-    score()    
-    death()
-    deathBigZumbi()
-    myReq = requestAnimationFrame(updateGameArea);
-    // gameOver()
-    console.log(life)
+  // } else if (attackStatus && count % 2 === 0) {
+  //   attackImg.src = 'Img/Attack1.png';
+  //   ninja.drawPlayer(attackImg);
+  // } else if (attackStatus && frame % 3 === 0) {
+  //   attackImg.src = 'Img/Attack3.png';
+  //   ninja.drawPlayer(attackImg);
+  // } else if (attackStatus && frame % 4 === 0) {
+  //   attackImg.src = 'Img/Attack5.png';
+  //   ninja.drawPlayer(attackImg);
+  // } else if (attackStatus && frame % 5 === 0) {
+  //   attackImg.src = 'Img/Attack7.png';
+  //   ninja.drawPlayer(attackImg);
+  } else {
+    attackImg.src = 'Img/Attack6.png';
+    ninja.drawPlayer(attackImg);
   }
-
+  machineBigZumbi()
+  machineZumbi()
+  frame += 1;
+  score()    
+  death()
+  deathBigZumbi()
   myReq = requestAnimationFrame(updateGameArea);
+  // gameOver();
+};
 
-
+myReq = requestAnimationFrame(updateGameArea);
